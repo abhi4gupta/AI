9b6eba7a4bc59e2dc9269cc11db3d75caac8cebd
abhi4gupta <47221030+abhi4gupta@users.noreply.github.com>
2021-04-16 14:34:02 +0530

    Add files via upload

---
+++ AI_lab2/code2.cpp
@@ -0,0 +1,107 @@
+/*Implement the dijkstra's algorithm on a graph. The vertices have "string" names (e.g. CC1, CC2, LT, etc.). 
+The edges along with the weights also have a "string" name (e.g. ringRoad, linkRoad, etc). 
+The shortest path from the source to all vertices should be printed as a string of vertex and edge names on 
+the shortest path (e.g. CC1 linkRoad CC2, ringRoad, LT, etc). 
+Implement the solution using both a heap as a data structure and an array as a priority queue as a data structure. */
+#include<bits/stdc++.h> 
+#define int long long
+using namespace std; 
+# define INF 1e18 
+typedef pair<int, int> iPair; 
+ 
+void addEdge(int u, int v, int wt,vector <pair<int, int> > adj[]) 
+{ 
+    adj[v].push_back(make_pair(u, wt)); 
+    adj[u].push_back(make_pair(v, wt)); 
+   
+} 
+int mind(vector<int>&dist, vector<bool>&visited,int V) 
+{ 
+    int min = INT_MAX, min_index; 
+  
+    for (int v = 0; v < V; v++) 
+        if (visited[v] == false && dist[v] <= min) 
+            min = dist[v], min_index = v; 
+  
+    return min_index; 
+} 
+
+void dijpriority( int V, int src,vector <pair<int, int> > adj[],vector<int> &dist,vector<int> &par) 
+{ 
+    vector<bool> visited(V,false);
+    dist[src] = 0; 
+
+    for(int i=0;i<V-1;i++){
+        int u=mind(dist,visited,V);
+        for (auto x : adj[u]) 
+        {
+            int v = x.first; 
+            int weight = x.second;  
+            if (dist[v] > dist[u] + weight&&!visited[v]) 
+            { 
+                dist[v] = dist[u] + weight; 
+                par[v]=u;
+            } 
+           
+        } 
+
+    }
+        
+} 
+signed main(){
+   #ifndef ONLINE_JUDGE
+   freopen("D:/input.txt","r",stdin);
+   freopen("D:/output.txt","w",stdout);
+  #endif
+    int n ,m;
+    cout<<"no of vertex and edges"<<endl;
+    cin>>n>>m;
+    vector<int> dist(n+1, INF);
+    vector <pair<int, int> > adj[n+1];
+    vector<int> par(n+1,0);
+    map<int, string> mpVtoS;
+    map<string,int>  mpStoV;
+    map<int,map<int,string>> mk;
+    
+    cout<<" enter source destination edge name and weight respectively"<<endl;
+    int k=0;
+    for(int i=0;i<m;i++){
+           string src,des,edgename;
+           int weight;
+           cin>>src>>des>>edgename>>weight;
+           if(!mpStoV.count(src)){
+              mpStoV[src]=k;
+              mpVtoS[k++]=src;
+           }
+           if(!mpStoV.count(des)){
+             mpStoV[des]=k;
+             mpVtoS[k++]=des;
+           }
+        mk[mpStoV[src]][mpStoV[des]]=edgename;
+        mk[mpStoV[des]][mpStoV[src]]=edgename;
+        addEdge(mpStoV[src],mpStoV[des],weight,adj);
+    }
+    cout<<"enter source name"<<endl;
+    string source;
+    cin>>source;
+    par[mpStoV[source]]=-1;
+    dijpriority(n,mpStoV[source],adj,dist,par);
+    cout<<"list of source name\n";
+    for(int i=0;i<n;i++) cout<<mpVtoS[i]<<endl;
+    for(int i=0;i<n;i++){
+        if(i!=mpStoV[source]){
+            cout<<"path of "<< mpVtoS[i]<<endl;
+            int pt=i;
+            while(pt!=-1){
+                if(par[pt]==-1) 
+                cout<<mpVtoS[pt];
+                else
+                cout<<mpVtoS[pt]<<" -- "<<mk[pt][par[pt]]<<" --> ";
+                pt=par[pt];
+            }
+            cout<<endl;
+        }
+    }
+    
+
+}
